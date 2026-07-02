831b2173a5c00cddd1a2a4570c3c9ce8c7b18694
Julio César <zzantares@gmail.com>
2026-07-02 01:09:25 -0600

    chore: drop unusable config files
    these files do not resolve for downstream users no need to have them

--- config/nvim/ftplugin/opentofu.lua
+++
@@ -1,9 +0,0 @@
-vim.opt_local.tabstop = 2
-vim.opt_local.softtabstop = 2
-vim.opt_local.shiftwidth = 2
-vim.opt_local.expandtab = true
-
--- syntax highlighting should match that one of terraform files
-vim.opt_local.syntax = "terraform"
-
-vim.bo.commentstring = "# %s"
